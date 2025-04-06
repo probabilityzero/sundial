b51c96cfb5c53b6a2bf9692dc77e994245387776
Prime <primecortex@outlook.com>
2025-04-06 08:29:33 +0600

    ​

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -14,7 +14,7 @@ function HomePage() {
         <div className="w-full max-w-5xl mx-auto px-4 flex justify-center">
         <DashboardSession />
         </div>
-        <div className="w-full max-w-5xl mx-auto px-4 sm:px-8 md:px-8">
+        <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 md:px-8">
         <DashboardGoal />
         </div>
     </motion.div>
